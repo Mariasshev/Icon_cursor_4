508f90152e406cf3832257c7ed0bfcd8ac2521ef
Maria Shevchenko <sevcenkomaria72@gmail.com>
2024-02-08 12:29:21 +0200

    Добавьте файлы проекта.

---
+++ Resources/resource.h
@@ -0,0 +1,23 @@
+//{{NO_DEPENDENCIES}}
+// ?????????? ????, ????????? ? Microsoft Visual C++.
+// ???????????? Resources.rc
+//
+#define IDC_CURSOR1                     101
+#define IDC_CURSOR2                     102
+#define IDI_ICON1                       103
+#define IDI_ICON2                       104
+#define IDC_CURSOR3                     105
+#define IDC_CURSOR4                     106
+#define IDI_ICON3                       107
+#define IDI_ICON4                       108
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
